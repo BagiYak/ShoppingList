55c7593d1f3c4b1a571046c3e5d41ffcbbe5b527
Bagdat Yakushev <bagdat.yakushev@gmail.com>
2022-06-06 00:23:44 +0600

    android/feature-14: xml templates for ShopItemActivity

---
+++ app/src/main/java/com/example/androidkotlinprofessionallevel/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidkotlinprofessionallevel.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidkotlinprofessionallevel.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
